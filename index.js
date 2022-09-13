83c73c8bc9a1e323d41e7590bc311adb2b9038c8
Dave Greber <davegreber@gmail.com>
2022-09-13 11:52:40 -0400

    added javascript message

---
+++ index.js
@@ -0,0 +1,8 @@
+function addingEventListener() {
+    const input = document.getElementById('headshot');
+    input.addEventListener('click', function() {
+      alert('you have just entered the matrix');
+    });
+}
+
+addingEventListener()
